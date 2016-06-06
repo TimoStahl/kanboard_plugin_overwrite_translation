e92b793a6fd21ca8327187c6d7a2c9f42080399f
BlueTeck <tili2@gmx.de>
2016-06-06 09:21:16 +0200

    add website

--- Plugin.php
+++ Plugin.php
@@ -5,33 +5,35 @@ namespace Kanboard\Plugin\Overwrite_translation;
 use Kanboard\Core\Plugin\Base;
 use Kanboard\Core\Translator;
 
-class Plugin extends Base
-{
-    public function initialize()
-    {
+class Plugin extends Base {
+
+    public function initialize() {
         // nothing
     }
-    
-    public function onStartup()
-    {
+
+    public function onStartup() {
         // load Translation
-        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
+        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__ . '/Locale');
     }
-    
-    public function getPluginName()
-    {
+
+    public function getPluginName() {
         return 'Overwrite Translation';
     }
-    public function getPluginDescription()
-    {
+
+    public function getPluginDescription() {
         return t('Overwrite default translations.');
     }
-    public function getPluginAuthor()
-    {
+
+    public function getPluginAuthor() {
         return 'BlueTeck';
     }
-    public function getPluginVersion()
-    {
-        return '1.0.29.0';
+
+    public function getPluginVersion() {
+        return '1.0.29.1';
     }
+
+    public function getPluginHomepage() {
+        return 'https://github.com/BlueTeck/kanboard_plugin_overwrite_translation';
+    }
+
 }
